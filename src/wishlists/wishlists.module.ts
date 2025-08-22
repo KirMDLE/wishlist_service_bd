ef7076e146d93aece39654547edad5d1da96ab51
KirMDLE <ciwbkbd@gmail.com>
2025-08-22 23:10:14 +0700

    fixed module wishlist

--- src/wishlists/wishlists.module.ts
+++ src/wishlists/wishlists.module.ts
@@ -1,26 +1,37 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { WishlistsController } from './wishlists.controller';
 import { WishlistsService } from './wishlists.service';
-import { WishlistItem } from './entities/wishlist-item.entity';
 import { Wishlist } from './entities/wishlist.entity';
-import { User } from 'src/users/entities/user.entity';
-import { CreateWishlistUseCase } from './application/usecases/create-wishlist.usecase';
-import { WishlistRepository } from './repositories/wishlist.repository';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { WishlistItem } from './entities/wishlist-item.entity';
 import { UsersModule } from 'src/users/users.module';
+import { CreateWishlistUseCase } from './application/usecases/create-wishlist.usecase';
+import { GetWishlistsUseCase } from './application/usecases/get-wishlists.usecase';
+import { UpdateWishlistUseCase } from './application/usecases/update-wishlist.usecase';
+import { DeleteWishlistUseCase } from './application/usecases/delete-wishlist.usecase';
+import { WishlistRepository } from './repositories/wishlist.repository';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Wishlist, WishlistItem,]),
-    UsersModule
+    TypeOrmModule.forFeature([Wishlist, WishlistItem]),
+    UsersModule,
   ],
   controllers: [WishlistsController],
-  providers: [WishlistsService, CreateWishlistUseCase, WishlistRepository],
+  providers: [
+    WishlistsService,
+    WishlistRepository,
+    CreateWishlistUseCase,
+    GetWishlistsUseCase,
+    UpdateWishlistUseCase,
+    DeleteWishlistUseCase,
+  ],
   exports: [
     WishlistsService,
+    WishlistRepository,
     CreateWishlistUseCase,
-    TypeOrmModule ,WishlistRepository
-  ],  
-  
+    GetWishlistsUseCase,
+    UpdateWishlistUseCase,
+    DeleteWishlistUseCase,
+  ],
 })
 export class WishlistsModule {}
