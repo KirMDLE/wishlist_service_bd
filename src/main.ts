f0bae7a93e4497d8af8a586f71b6bf82fac34b96
KirMDLE <ciwbkbd@gmail.com>
2025-08-22 23:18:57 +0700

    upd main

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const PORT = process.env.PORT ?? 3000; 
+  const PORT = process.env.PORT ?? 3000;
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,     
+      forbidNonWhitelisted: true, 
+      transform: true,      
+    }),
+  );
+
+  app.enableCors();
 
   await app.listen(PORT);
 }
+
+bootstrap();
