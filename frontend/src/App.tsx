4861be710374bc9ed39c9ac2f4763bd3002c5750
KirMDLE <ciwbkbd@gmail.com>
2025-09-01 22:55:51 +0700

    started filling register page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,22 +1,11 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
+import React from 'react';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Header />
-      <main style={{ padding: "1rem" }}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
+    <div>
+      <h1>Wishlist Service</h1>
+    </div>
   );
-}
+};
 
 export default App;
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,4 +1,26 @@
+
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import api from '../api';
+
+
 export default function Register() {
-    return <h1>Register Page</h1>;
-  }
-  
+  const Register: React.FC = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const navigate = useNavigate();}
+ 
+
+
+
+
+
+
+
+    return (
+      <form onSubmit={handleSubmit}>
+       <label className="">First Name</label>
+
+)}
